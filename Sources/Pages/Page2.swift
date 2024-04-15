3c1dd52edb56ba225dea5febba9b16fb4f09256e
mpot05 <146374743+mpot05@users.noreply.github.com>
2024-04-15 10:54:36 -0400

    Create Page2.swift

---
+++ Sources/Pages/Page2.swift
@@ -0,0 +1,16 @@
+import Foundation
+import Ignite
+
+struct Page2: StaticPage {
+    var title = "Page 2"
+    
+    func body(context: PublishingContext) -> [BlockElement] {
+        Group {
+            Text("A second page")
+                .font(.title1)
+        }
+        .padding(.top,.extraLarge)
+        .margin()
+    }
+    
+}
