358502878145486ff1f14e6f293a05ed65c78c9c
mpot05 <146374743+mpot05@users.noreply.github.com>
2024-04-16 11:19:06 -0400

    apparently not all syntax highlighters work

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -19,8 +19,7 @@ struct ExampleSite: Site {
     var baseTitle = " – My Awesome Site"
     var url = URL("https://www.example.com")
     var builtInIconsEnabled = true
-    var syntaxHighlighters = [SyntaxHighlighter.swift, .python, .ruby, .java, .c, .bash, .cLike, .css, .cSharp, .appleScript, .cPlusPlus, .dart, .git, .go, .html, .javaScript, .json, .kotlin, .markdown, .markup, .markupTemplating, .objectiveC, .perl, .php, .rust, .sql, .typeScript, .webAssembly, .yaml]
-
+    var syntaxHighlighters = [SyntaxHighlighter.swift, .python, .ruby, .java, .c, .cLike, .cPlusPlus, .cSharp, .appleScript, .typeScript, .bash, .css, .dart, .git, .go, .javaScript, .kotlin, .markdown, .markup, .markupTemplating, .objectiveC, .perl, .php, .rust, .sql, .yaml]
     var author = "John Appleseed"
 
     var homePage = Home()
