20bf296bf376f379155fdba01107a8d73f55d763
Michael Potter <mpotter0905@gmail.com>
2024-04-18 10:33:39 -0400

    made css and js work

--- Sources/Themes/MainTheme.swift
+++ Sources/Themes/MainTheme.swift
@@ -5,9 +5,15 @@ struct MyTheme: Theme {
     func render(page: Page, context: PublishingContext) -> HTML {
         HTML {
             Head(for: page, in: context)
-
+            Head {
+                MetaLink(href: "/Build/css/bootstrap-icons.min.css", rel: .stylesheet)
+                MetaLink(href: "/Build/css/bootstrap.min.css", rel: .stylesheet)
+                MetaLink(href: "/Build/css/prism-default-dark.css", rel: .stylesheet)
+            }
             Body {
                 page.body
+                Script(file: "/Build/js/bootstrap.bundle.min.js")
+                Script(file: "/Build/js/syntax-highlighting.js")
             }
         }
     }
